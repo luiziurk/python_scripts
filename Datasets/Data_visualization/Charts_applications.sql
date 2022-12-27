688f1689cb3e81b43e7744f738e3bb8f38092bc5
Luiz Iurk <iurk.lf@gmail.com>
2022-12-27 11:55:55 +0100

    latest changes

---
+++ Datasets/Data_visualization/Charts_applications.sql
@@ -0,0 +1,9 @@
+Summary
+In this lesson, you have learned to code the following types of visualizations:
+
+- scatterplots: show the relationship between two quantitative variables
+- clustered bar charts: show the relationship between two qualitative variables
+- heat maps: used as 2D histograms and bar charts
+- violin and box plots: show the relationship between one quantitative and one quantitative variable
+- faceting: adapt univariate plots to bivariate datavariate ex
+- line plots: show changes in value across time
--- work_pdf_files.py
+++ work_pdf_files.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 from PyPDF2 import PdfReader
 from datetime import datetime
-import random as rnd
+import numpy as np
 
 #%%
 # VARIABLES
@@ -24,7 +24,6 @@ detail_head_foot = {
 
 # %%
 # MAIN
-file1 = target_dir + "\\" + os.listdir()[0]
 
 files = []
 for i, j in enumerate(os.listdir()):
@@ -53,6 +52,7 @@ def fetch_summary(text):
         "DATE": [],
         "AMOUNT": [],
     }
+    print("Working the rest", flush=True)
     # It was found that the first 3 lines of the summary have no use and the 4th line is different from the other lines
     for i, j in enumerate(sum_final):
         if i == 3:
@@ -72,51 +72,13 @@ def fetch_summary(text):
             else:
                 expense_summary["AMOUNT"].append(l[2])
             expense_summary["DESCRIPTION"].append(" ".join(l[3:]))
-    return pd.DataFrame(expense_summary)
+        df = pd.DataFrame(expense_summary)
+        df["AMOUNT"] = df["AMOUNT"].astype(str).str.replace(".", "", regex=True)
+        df["AMOUNT"] = df["AMOUNT"].astype(str).str.replace(",", ".", regex=True)
+        df["AMOUNT"] = pd.to_numeric(df["AMOUNT"])
+        df["DATE"] = pd.to_datetime(df["DATE"])
 
-
-def fetch_expenses(text):
-
-    for i, j in enumerate(text.splitlines()):
-        if "DETAIL DES OPERATIONS DU" in j:
-            period_line = text.splitlines()[i]
-    edited_txt = text[
-        text.index("ANCIEN SOLDE") : text.index("NOUVEAU SOLDE")
-        + text[text.index("NOUVEAU SOLDE") :].index("\n")
-    ]
-    return edited_txt, period_line
-
-
-#%%
-print("Fetching the Summary Expense", flush=True)
-
-df_summary = pd.DataFrame({"DESCRIPTION": [], "DATE": [], "AMOUNT": [], "PERIOD": []})
-df_detailed = pd.DataFrame(
-    {
-        "DESCRIPTION": [],
-        "COUNTRY": [],
-        "DATE_SPENT": [],
-        "DATE_EFFECTIVE": [],
-        "AMOUNT": [],
-    }
-)
-
-for f in files:
-    # It is very important that the file name follows the same structure
-
-    date = f[-10:-6] + "/" + f[-6:-4] + "/01"
-    date = datetime.strptime(date, "%Y/%m/%d")
-    t = fetch_file(f)
-    df_sum_k = fetch_summary(t)
-    df_sum_k["PERIOD"] = date
-    df_summary = pd.concat([df_summary, df_sum_k])
-
-df_summary = df_summary.reset_index(drop=True)
-
-
-#%%
-
-print("Fetching the Detailed Expense", flush=True)
+    return df
 
 
 def fetch_expenses(text):
@@ -129,6 +91,7 @@ def fetch_expenses(text):
         text.index("ANCIEN SOLDE") : text.index("NOUVEAU SOLDE")
         + text[text.index("NOUVEAU SOLDE") :].index("\n")
     ]
+    edited_txt = edited_txt.replace(" ,", ",")
 
     period_start = period_line[
         period_line.index("DU") + 2 : period_line.index("AU")
@@ -158,27 +121,19 @@ def fetch_expenses(text):
     }
     for i, j in enumerate(edited_txt.splitlines()):
         if any(sign in j for sign in ["-", "+"]):
-            if any(item in j for item in detail_head_foot["FOOTER"]):
-                amount = j.split()[-1] + j.split()[-2]
-                if "00" in j.split()[0]:
-                    spe = period_end.strftime("%d/%m/%Y")
-                else:
-                    spe = (
-                        j.split()[0]
-                        + "/"
-                        + str(
-                            p_month_year["YEARS"][
-                                p_month_year["MONTHS"].index(int(j.split()[0][-2:]))
-                            ]
-                        )
+            amount = j.split()[-1] + j.split()[-2]
+
+            if (
+                any(
+                    item in j
+                    for item in (
+                        detail_head_foot["HEADER"]
+                        + detail_head_foot["FOOTER"]
+                        + detail_head_foot["PAYABLE"]
                     )
-                eff = spe
-                ctr = "BE"
-                desc = " ".join(j.split()[1:-2])
-
-            else:
-                amount = j.split()[-1] + j.split()[-2]
-
+                )
+                == False
+            ):
                 eff = (
                     j.split()[-3]
                     + "/"
@@ -199,15 +154,127 @@ def fetch_expenses(text):
                 )
                 ctr = j.split()[-5]
                 desc = " ".join(j.split()[:-5])
+
+            else:
+                if any(
+                    item in j
+                    for item in (
+                        detail_head_foot["HEADER"] + detail_head_foot["PAYABLE"]
+                    )
+                ):
+                    try:
+                        eff = (
+                            j.split()[-3]
+                            + "/"
+                            + str(
+                                p_month_year["YEARS"][
+                                    p_month_year["MONTHS"].index(
+                                        int(j.split()[-3][-2:])
+                                    )
+                                ]
+                            )
+                        )
+                    except:
+                        eff = first_period[0]
+
+                    desc = " ".join(j.split()[0:-3])
+                else:
+                    try:
+                        eff = (
+                            j.split()[0]
+                            + "/"
+                            + str(
+                                p_month_year["YEARS"][
+                                    p_month_year["MONTHS"].index(int(j.split()[0][-2:]))
+                                ]
+                            )
+                        )
+                    except:
+                        eff = first_period[1]
+                    desc = " ".join(j.split()[1:-2])
+                spe = eff
+                ctr = "BE"
+
             expense_details["DESCRIPTION"].append(desc)
             expense_details["COUNTRY"].append(ctr)
             expense_details["DATE_SPENT"].append(spe)
             expense_details["DATE_EFFECTIVE"].append(eff)
             expense_details["AMOUNT"].append(amount)
+    df = pd.DataFrame(expense_details)
 
-    return expense_details
+    df["DATE_SPENT"] = df["DATE_SPENT"].astype(np.datetime64)
+    df["DATE_EFFECTIVE"] = df["DATE_EFFECTIVE"].astype(np.datetime64)
+    df["AMOUNT"] = df["AMOUNT"].astype(str).str.replace(".", "", regex=True)
+    df["AMOUNT"] = df["AMOUNT"].astype(str).str.replace(",", ".", regex=True)
+    df["AMOUNT"] = df["AMOUNT"].astype(np.float_)
+    #df_detailed["AMOUNT"] = pd.to_numeric(df_detailed["AMOUNT"])
 
 
-rand_file = fetch_file(files[rnd.randint(0, len(files) - 1)])
 
-expenses = fetch_expenses(fetch_file(files[rnd.randint(0, len(files) - 1)]))
+    return df
+
+
+#%%
+print("Creating the Data Frames", flush=True)
+df_summary = pd.DataFrame({"DESCRIPTION": [], "DATE": [], "AMOUNT": [], "PERIOD": []})
+df_detailed = pd.DataFrame(
+    {
+        "DESCRIPTION": [],
+        "COUNTRY": [],
+        "DATE_SPENT": [],
+        "DATE_EFFECTIVE": [],
+        "AMOUNT": [],
+    }
+)
+
+print("Looping Over the Files", flush=True)
+for f in files:
+    # It is very important that the file name follows the same structure
+    print(f"Working on file {f} ", flush=True)
+
+    date = f[-10:-6] + "/" + f[-6:-4] + "/01"
+    date = datetime.strptime(date, "%Y/%m/%d")
+    t = fetch_file(f)
+    print("Fetching the Summary Expense", flush=True)
+    df_sum_k = fetch_summary(t)
+    print("Adding the Period", flush=True)
+    df_sum_k["PERIOD"] = date
+    print("Concatenating the data", flush=True)
+    df_summary = pd.concat([df_summary, df_sum_k])
+    print("Fetching Detailed Expenses", flush=True)
+    df_det_k = fetch_expenses(t)
+    print("Concatenating the data", flush=True)
+    df_detailed = pd.concat([df_detailed, df_det_k])
+
+
+print("Resetting the indexes", flush=True)
+df_summary = df_summary.reset_index(drop=True)
+df_detailed = df_detailed.reset_index(drop=True)
+# Discarding the Previous saldo Lines to avoid double charging
+df_summary_wo_previous = df_summary[
+    ~(
+        df_summary["DESCRIPTION"].str.startswith("Ancien")
+        | df_summary["DESCRIPTION"].str.startswith("Vorig")
+    )
+]
+df_summary_wo_previous["CUMSUM"] = df_summary_wo_previous["AMOUNT"].cumsum()
+df_summary_wo_previous["CUMSUM"] = df_summary_wo_previous["CUMSUM"].apply(
+    lambda x: round(x, 2)
+)
+
+# FIXING THE DOUBLE NAME IN DESCRIPTION
+for i, j in enumerate(df_detailed['DESCRIPTION']):
+    if (len(j.split()))>1 and j.split()[-1] == j.split()[-2]:
+        print(j, flush = True)
+        df_detailed["DESCRIPTION"][i] = " ".join(j.split()[0:-1])
+
+    else:
+        print("OK >>>",j, flush = True)
+        df_detailed["DESCRIPTION"][i] = j
+
+
+
+
+
+#%%
+# Exporting into a file
